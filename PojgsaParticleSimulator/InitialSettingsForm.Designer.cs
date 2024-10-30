908ad1e974c4b048095587033959f2be330dc203
Pudgey2 <gghazal2004@gmail.com>
2024-10-30 16:08:35 -0400

    Updated initial settings form

--- PojgsaParticleSimulator/InitialSettingsForm.Designer.cs
+++ PojgsaParticleSimulator/InitialSettingsForm.Designer.cs
@@ -28,10 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InitialSettingsForm));
+            this.SuspendLayout();
+            // 
+            // InitialSettingsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(452, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "InitialSettingsForm";
+            this.Text = "Main Menu";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
